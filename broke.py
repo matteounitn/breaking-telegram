6cd6159459d49351a5f41cbf4b3e6d3070efb761
matteounitn <32160291+matteounitn@users.noreply.github.com>
2024-03-01 11:59:56 +0100

    Finally fixed dependencies

--- broke.py
+++ broke.py
@@ -9,13 +9,12 @@ from pyrogram import Client
 import os.path
 
 with open("config.ini", "r") as f:
-    #we read the second and third line and split = to get the api_id and api_hash
-    file= f.readlines()
+    # we read the second and third line and split = to get the api_id and api_hash
+    file = f.readlines()
     api_id = file[1].split("=")[1].strip()
     api_hash = file[2].split("=")[1].strip()
 
 client = Client(name="default", api_id=api_id, api_hash=api_hash)
-
 
 
 @client.on_raw_update(group=-100)
@@ -23,6 +22,7 @@ def handler(client, update, users, chats):
     if isinstance(update, UpdateNewMessage) and not isinstance(
         update.message, MessageService
     ):
+        # print(update) # debug
         if (
             (
                 isinstance(update.message.media, MessageMediaDocument)
@@ -32,7 +32,12 @@ def handler(client, update, users, chats):
             and update.message.out is False
             and update.message.media.ttl_seconds is not None
         ):
-            message = client.get_messages(update.message.peer_id.user_id, update.message.id)
+            message = client.get_messages(
+                update.message.peer_id.user_id, update.message.id
+            )
+            print(
+                f"Message Received from userID {update.message.peer_id.user_id}. Saving!"
+            )
             text = (
                 f"__New Secret__\n__From__ {message.from_user.first_name} -"
                 f" [{message.from_user.id}](tg://user?id={message.from_user.id}) \n\n"
@@ -45,4 +50,5 @@ def handler(client, update, users, chats):
                 os.remove(path)
 
 
+print("Started! waiting for message...Press CTRL+C to stop.")
 client.run()
